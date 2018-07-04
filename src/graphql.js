38e6c7566cc7e5d97d360cccdfd90860a45ff81f
Lars <lars.berger@yahoo.com>
2018-07-04 16:10:51 +0200

    feat: added marlon's graphql changes

--- src/graphql.js
+++ src/graphql.js
@@ -1,12 +1,17 @@
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import config from './config';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloClient } from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { createHttpLink } from 'apollo-link-http';
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('token');
+  console.log('my token >', token);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
@@ -27,15 +32,36 @@ const defaultOptions = {
   },
 };
 
+const wsLink = new WebSocketLink({
+  uri: `${config.webSocketURI}/subscriptions`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      token: localStorage.getItem('token'),
+    },
+  },
+});
+
+// Create an http link:
 const httpLink = createHttpLink({
   uri: `${config.serverURI}/graphql`,
 });
 
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  authLink.concat(wsLink),
+  authLink.concat(httpLink)
+);
+
+const cache = new InMemoryCache();
 export default new ApolloClient({
-  // Provide the URL to the API server.
-  link: authLink.concat(httpLink),
-  // Using a cache for blazingly
-  // fast subsequent queries.
-  cache: new InMemoryCache(),
+  link,
+  cache,
   defaultOptions: defaultOptions,
 });
