01f6af5d0d3b1ff87a3c6ce0c7ee11a185f75634
Marlon Becker Santos <marlon@MBP-de-Marlon.lan>
2018-07-05 00:06:56 +0200

    changed backend url for develop

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  serverURI: 'http://192.168.1.218:2017',
-  webSocketURI: 'ws://192.168.1.218:2017',
+  serverURI: 'http://192.168.1.218:2018',
+  webSocketURI: 'ws://192.168.1.218:2018',
 };
